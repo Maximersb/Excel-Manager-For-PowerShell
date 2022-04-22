0e1a7419c3f9f857d8df70312cc20c0a21663060
Maxime <94387211+Maximersb@users.noreply.github.com>
2022-04-22 09:13:42 +0200

    Worksheets methods added + Save as + Delete method
    Delete need to be use at the end of script to prevent excel running in background

--- ExcelManager.psm1
+++ ExcelManager.psm1
@@ -3,15 +3,26 @@ class ExcelManager{
     hidden [ValidateNotNullOrEmpty()][System.__ComObject]$excelApp
     hidden [ValidateNotNullOrEmpty()][System.MarshalByRefObject]$workBook
     
-
-    ExcelManager() {
+    ###Constructor
+    ExcelManager() { 
         $this.excelApp = New-Object -ComObject excel.application
         $this.workBook = $this.excelApp.workbooks.add()
         $this.workSheetList= @($this.workBook.worksheets.item(1))
-        if($null -ne (Get-Process -Name EXCEL -ErrorAction SilentlyContinue -ErrorVariable ProcessError))
+        if($null -ne (Get-Process -Name EXCEL -ErrorAction SilentlyContinue -ErrorVariable ProcessError)){
             Stop-Process -Name EXCEL
+        }
     }
 
+    ExcelManager([System.__ComObject] $excelApp) {
+        $this.excelApp = $excelApp
+        $this.workBook = $this.excelApp.workbooks.add()
+        $this.workSheetList= @($this.workBook.worksheets.item(1))
+        if($null -ne (Get-Process -Name EXCEL -ErrorAction SilentlyContinue -ErrorVariable ProcessError)){
+            Stop-Process -Name EXCEL
+        }
+    }
+
+    #worksheets methods
     [void] addWorkSheet(){
         $this.workSheetList += $this.workBook.worksheet.add([System.Reflection.Missing]::Value,$this.workSheetList[$this.workSheetList.Lenght-1])
     }
@@ -25,24 +36,31 @@ class ExcelManager{
         $this.workSheetList[$index].name = $name
     }
 
-    [bool] saveAs([string] $path, [string] $name){
-        try{
-            $this.excelApp.displayalerts = $false
-            $this.workBook.Saveas($path + \ + $name)
-            $this.excelApp.displayalerts = $true
-            return $True
-        }catch{
-            return $False
-        }
-    }
-
     [System.MarshalByRefObject] getWorkSheet([int] $index){
         return $this.workSheetList[$index]
     }
 
-    [void] endManagment(){
-        $excelApp.Quit()| Out-Null
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excelApp)| Out-Null
+    [System.Collections.ArrayList] getWorkSheet(){
+        return $this.workSheetList
+    }
+
+    ###Save your files with the path and the name
+    [bool] saveAs([string] $path, [string] $name){
+        try{
+            $this.excelApp.displayalerts = $false
+            $this.workBook.Saveas($path + "\" + $name)
+            $this.excelApp.displayalerts = $true
+            return $True
+        }
+        catch{
+            return $False
+        }
+    }
+
+    ###Always use this methods when you have finish with excel
+    [void] delete(){
+        $this.excelApp.Quit()| Out-Null
+        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($this.excelApp)| Out-Null
         Remove-Variable excelApp | Out-Null
     }
 }
